ed760c478fa4a06b3301547e20d35607e3f031ce
unknown <sivalankalaxmi809@gmail.com>
2024-09-20 19:54:36 +0530

    Few changes made

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { UpdatelistComponent } from './updatelist/updatelist.component';
+import { ContentComponent } from './content/content.component';
 
 export const routes: Routes = [
     {path:'our-story', component: OurStoryComponent},
     {path: 'contact', component: ContactComponent},
     {path: 'contactlist', component: ContactListComponent},
-    {path: 'update/:id', component: UpdatelistComponent}
+    {path: 'update/:id', component: UpdatelistComponent},
+    {path: 'content', component: ContentComponent}
 ];
